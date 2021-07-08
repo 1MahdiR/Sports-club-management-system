e41cebb5044520a9413ddb169acba13ac1882c19
m r <a.m.rasouli.n@gmail.com>
2021-07-08 08:16:25 +0430

    Added show_menu and clear_screnn methods to main class

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
-
-
 public class Main {
+
+    private static void show_menu() {
+        System.out.println("#~~~~~~~~~~~~~~~~~~~~~~~~#");
+        System.out.println("| Sports club management |");
+        System.out.println("|                        |");
+        System.out.println("| 1.Make a reservation   |");
+        System.out.println("| 2.Add a new equipment  |");
+        System.out.println("| 3.Add a new client     |");
+        System.out.println("| 4.Edit equipments      |");
+        System.out.println("| 5.Edit reservations    |");
+        System.out.println("| 6.Exit                 |");
+        System.out.println("|                        |");
+        System.out.println("#~~~~~~~~~~~~~~~~~~~~~~~~#");
+        System.out.printf("\n>");
+    }
+
+    private static void clear_screen() {
+        System.out.print("\033[H\033[2J");
+    }
+
+
 
     public static void main(String[] args) {
 
